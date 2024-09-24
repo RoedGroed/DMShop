7e482244ffc1a5fb7464b8beb99e6e8f68677a10
andersthomsen92 <doomknight92@gmail.com>
2024-09-24 22:20:48 +0200

    Get All Products implemented
    Still need:
    - frontend.
    - unit test.
    API tested with Swagger

--- server/API/Controllers/ProductController.cs
+++ server/API/Controllers/ProductController.cs
@@ -1,6 +1,21 @@
-﻿namespace API.Controllers;
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Service;
 
-public class ProductController
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController(IDMShopService service, IOptionsMonitor<AppOptions> options) : ControllerBase
 {
+
+    [HttpGet]
+    [Route("")]
+    public ActionResult<List<Paper>> GetAllPapers()
+    {
+        var papers = service.GetAllPapers();
+        return Ok(papers);
+    }
     
 }
