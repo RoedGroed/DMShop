fa1c8695760e0e567ffb18ea703c69ba0c5eadb9
RoedGroed <jonasroer92@gmail.com>
2024-10-11 01:38:30 +0200

    updated lists and buttons to be similar across products and order pages

--- client/src/components/Order/CustomerOrderHistory.tsx
+++ client/src/components/Order/CustomerOrderHistory.tsx
@@ -45,12 +45,15 @@ const CustomerOrderHistory = () => {
     };
 
     return (
-        <div className="min-h-screen bg-customBlue flex flex-col items-center justify-start pt-16">
-            <ul className="space-y-2 w-3/4">
+        <div className="min-h-screen bg-customBlue flex flex-col items-center p-4 space-y-8">
+            <h1 className="text-2xl font-bold text-center text-white">
+                Order History
+            </h1>
+            <ul className="space-y-2 w-full max-w-5xl">
                 {orders.map((order) => (
                     <li
                         key={order.id}
-                        className="p-2 bg-gray-900 rounded-lg flex items-center text-white text-lg cursor-pointer"
+                        className="p-2 bg-gray-900 rounded-lg flex items-center text-white text-base cursor-pointer"
                         onClick={() => handleOnClickOrder(order.id)}
                     >
                         {/* Order Information */}
@@ -67,21 +70,22 @@ const CustomerOrderHistory = () => {
                         <p className="w-2/12">
                             <span className="font-bold">Total: $</span> {order.totalAmount.toFixed(2)}
                         </p>
+
                         {/* Status */}
                         <div className="w-2/12 flex items-center justify-end space-x-2">
-                            <span
-                                className={`font-bold px-4 py-2 rounded w-28 text-center ${
-                                    order.status === "delivered"
-                                        ? "bg-green-500"
-                                        : order.status === "processing"
-                                            ? "bg-yellow-500"
-                                            : order.status === "cancelled"
-                                                ? "bg-red-500"
-                                                : "bg-gray-500"
-                                } text-white`}
-                            >
-                                {order.status}
-                            </span>
+                        <span
+                            className={`font-bold px-2 py-1 rounded text-center ${
+                                order.status === "delivered"
+                                    ? "bg-green-500"
+                                    : order.status === "processing"
+                                        ? "bg-yellow-500"
+                                        : order.status === "cancelled"
+                                            ? "bg-red-500"
+                                            : "bg-gray-500"
+                            } text-white`}
+                        >
+                            {order.status}
+                        </span>
                         </div>
                     </li>
                 ))}
@@ -89,10 +93,11 @@ const CustomerOrderHistory = () => {
 
             {/* Modal to order details */}
             {modalOpen && selectedOrder && (
-                <OrderModal order={selectedOrder} onClose={handleCloseModal} />
+                <OrderModal order={selectedOrder} onClose={handleCloseModal}/>
             )}
         </div>
     );
+
 };
 
 export default CustomerOrderHistory;
--- client/src/components/Order/OrdersList.tsx
+++ client/src/components/Order/OrdersList.tsx
@@ -62,15 +62,18 @@ const OrdersList = () => {
                 }
             });
     };
-    
-    return (
-        <div className="min-h-screen bg-customBlue flex flex-col items-center justify-start pt-16">
-            <ul className="space-y-2 w-3/4">
-                {orders.map((order) => (
-                    <li key={order.id} className="p-2 bg-gray-900 rounded-lg flex items-center text-white text-lg cursor-pointer"
-                    onClick={() => handleOnClickOrder(order.id)}
-                    >   
 
+    return (
+        <div className="min-h-screen bg-customBlue flex flex-col items-center p-4 space-y-8">
+            <h1 className="text-2xl font-bold text-center text-white">
+                Orders
+            </h1>
+            <ul className="space-y-2 w-full max-w-5xl">
+                {orders.map((order) => (
+                    <li key={order.id}
+                        className="p-2 bg-gray-900 rounded-lg flex items-center text-white text-base cursor-pointer"
+                        onClick={() => handleOnClickOrder(order.id)}
+                    >
                         {/* Order Information */}
                         <p className="w-2/12">
                             <span className="font-bold">Order:</span> {order.id}
@@ -83,24 +86,24 @@ const OrdersList = () => {
                             <p>Ordered: {new Date(order.orderDate).toLocaleString()}</p>
                             <p>Delivery: {order.deliveryDate}</p>
                         </div>
-                        
+
                         <p className="w-2/12">
-                            <span className="font-bold">Total: $</span>{order.totalAmount.toFixed(2)} 
+                            <span className="font-bold">Total: $</span>{order.totalAmount.toFixed(2)}
                         </p>
 
                         {/* Status */}
                         <div className="w-2/12 flex items-center justify-end space-x-2">
-                            <span className={`font-bold px-4 py-2 rounded w-28 text-center ${
-                                order.status === "delivered"
-                                    ? "bg-green-500"
-                                    : order.status === "processing"
-                                        ? "bg-yellow-500"
-                                        : order.status === "cancelled"
-                                            ? "bg-red-500"
-                                            : "bg-gray-500"
-                            } text-white`}>
-                                {order.status}
-                            </span>
+                        <span className={`font-bold w-full h-full rounded text-center ${
+                            order.status === "delivered"
+                                ? "bg-green-500"
+                                : order.status === "processing"
+                                    ? "bg-yellow-500"
+                                    : order.status === "cancelled"
+                                        ? "bg-red-500"
+                                        : "bg-gray-500"
+                        } text-white`}>
+                            {order.status}
+                        </span>
                             <div
                                 className="w-1/6 justify-end space-x-2"
                                 onClick={(e) => e.stopPropagation()}>
@@ -116,7 +119,7 @@ const OrdersList = () => {
             </ul>
 
             {/* Pagination controls */}
-            <div className="flex justify-between mt-4 w-3/4">
+            <div className="flex justify-between mt-4 w-full max-w-5xl">
                 <button
                     disabled={page === 0}
                     onClick={() => setPage(page - 1)}
@@ -131,10 +134,10 @@ const OrdersList = () => {
                     Next
                 </button>
             </div>
-            
+
             {/* Modal to order details */}
             {modalOpen && selectedOrder && (
-                <OrderModal order={selectedOrder} onClose={handleCloseModal} />
+                <OrderModal order={selectedOrder} onClose={handleCloseModal}/>
             )}
         </div>
     );
